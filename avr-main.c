84e6a55e3e1e89dd575a017b0250ac63634c110b
Daniel-ayal <82984235+Daniel-ayal@users.noreply.github.com>
2021-05-09 16:26:29 -0500

    Update avr-main.c
    entrega daniel ayala

--- avr-main.c
+++ avr-main.c
@@ -1,17 +1,21 @@
 /*
  * File:   avr-main.c
- * Author: rlmmx
+ * Author: daniel ayala 
  *
  * Created on 27 de abril de 2021, 09:31 PM
  */
 
 
 #include <avr/io.h>
+#define F_CPU 100000UL
+#include <util/delay.h>
 
 int main(void) {
-    
-    DDRA =  0;
-    /* Replace with your application code */
+    DDRB = 0xFF;
     while (1) {
+        PORTB = 0b00000001;
+        _delay_ms(2000);
+        PORTB = 000000000;
+        _delay_ms(2000);
     }
 }
