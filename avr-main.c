16072e36fd1286b128ed378feee931d45913aef7
Dayel35 <83044818+Dayel35@users.noreply.github.com>
2021-05-13 16:24:43 -0500

    DayelMorales_Practica1

--- avr-main.c
+++ avr-main.c
@@ -1,17 +1,25 @@
 /*
  * File:   avr-main.c
- * Author: rlmmx
+ * Author: Dayel Morales
  *
  * Created on 27 de abril de 2021, 09:31 PM
  */
 
 
+#include <xc.h>
+#include <util/delay.h>
 #include <avr/io.h>
-
+#define F_CPU 10000000UL
 int main(void) {
     
-    DDRA =  0;
-    /* Replace with your application code */
+    DDRB = 0X01;
+    PORTB = 0b00000001;
+    
+    
     while (1) {
+        PORTB =0b00000001;
+        _delay_ms(2000);
+        PORTB = 0b00000001;
+        _delay_ms(200);
     }
 }
